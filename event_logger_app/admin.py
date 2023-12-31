b8647466c76e265170805bc8a3d28c17bcb435ef
eli.goldin <igoldin74@gmail.com>
2023-12-31 17:20:59 -0600

    Fixed a bug when admin dashboard wasn't displaying event name and a bug when non-string was returned on adding an event entry.

--- event_logger_app/admin.py
+++ event_logger_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from event_logger_app.models import Event, EventEntry
+
+admin.site.register(Event)
+admin.site.register(EventEntry)
--- event_logger_app/models.py
+++ event_logger_app/models.py
@@ -3,9 +3,23 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     """An event that occurs and being logged"""
-    event_type = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     event_timestamp = models.DateTimeField(auto_now_add=True)
 
-def __str__(self):
-    """return a self representation of an event"""
-    return self.event_type
+    def __str__(self):
+        """return a self representation of an event"""
+        return self.name
+
+
+class EventEntry(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    client_ip = models.GenericIPAddressField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return self.event.name
+    
+
