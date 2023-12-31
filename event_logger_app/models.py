a46349c175d7a79618d6de8c9fc0dff8225d739e
eli.goldin <igoldin74@gmail.com>
2023-12-31 12:03:58 -0600

    Created 'Event' model and a migration for it

--- event_logger_app/models.py
+++ event_logger_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    """An event that occurs and being logged"""
+    event_type = models.CharField(max_length=200)
+    event_timestamp = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    """return a self representation of an event"""
+    return self.event_type
